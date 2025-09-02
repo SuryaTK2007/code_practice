d1e5500d165da88d5ce071131db042b3b84aea22
Surya <suryatk2007@gmail.com>
2025-09-03 00:31:06 +0530

    71A: Way Too Long Words

--- code_forces.cpp
+++ code_forces.cpp
@@ -1,38 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int w;
-    cin >> w;
-    if (w % 2 == 0 && w > 2)
-        cout << "YES\n";
-    else
-        cout << "NO\n";
-    return 0;
+int main(){
+    int n;
+    vector<string> s;
+    string s1;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>s1;
+        s.push_back(s1);
+    }
+    for(auto &s2: s){
+        if(s2.size()<=10){
+            cout<<s2<<endl;
+        }
+        else{
+            cout<<s2[0];
+            cout<<s2.size()-2;
+            cout<<s2[s2.size()-1]<<endl;
+        }
+    }
 }
-
 /*
-
-One hot summer day Pete and his friend Billy decided to buy a watermelon. They chose the biggest and the ripest one, in their opinion. 
-After that the watermelon was weighed, and the scales showed w kilos. They rushed home, dying of thirst, and decided to divide the berry, 
-however they faced a hard problem.
-Pete and Billy are great fans of even numbers, that's why they want to divide the watermelon in such a way that each of the two parts weighs even number of kilos,
-at the same time it is not obligatory that the parts are equal. The boys are extremely tired and want to start their meal as soon as possible, 
-that's why you should help them and find out, if they can divide the watermelon in the way they want. For sure, each of them should get a part of positive weight.
+Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
+Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
+This abbreviation is made like this: we write down the first and the last letter of a word and between them we write the number of letters between 
+the first and the last letters. That number is in decimal system and doesn't contain any leading zeroes.
+Thus, "localization" will be spelt as "l10n", and "internationalization» will be spelt as "i18n".
+You are suggested to automatize the process of changing the words with abbreviations. At that all too long words should be replaced by the abbreviation 
+and the words that are not too long should not undergo any changes.
 
 Input
-The first (and the only) input line contains integer number w (1 ≤ w ≤ 100) — the weight of the watermelon bought by the boys.
+The first line contains an integer n (1 ≤ n ≤ 100). Each of the following n lines contains one word. All the words consist of lowercase Latin letters and 
+possess the lengths of from 1 to 100 characters.
 
 Output
-Print YES, if the boys can divide the watermelon into two parts, each of them weighing even number of kilos; and NO in the opposite case.
+Print n lines. The i-th line should contain the result of replacing of the i-th word from the input data.
 
 Examples
-Input
-8
-Output
-YES
 
-Note
-For example, the boys can divide the watermelon into two parts of 2 and 6 kilos respectively (another variant — two parts of 4 and 4 kilos).
+Input
+4
+word
+localization
+internationalization
+pneumonoultramicroscopicsilicovolcanoconiosis
+
+Output
+word
+l10n
+i18n
+p43s
 
 */

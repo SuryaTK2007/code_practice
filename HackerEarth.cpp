91bc8c82713ea7bf6ba77520342441496c733162
Surya <suryatk2007@gmail.com>
2025-09-03 00:59:43 +0530

    Monk and rotation - Right rotation of an array

---
+++ HackerEarth.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        k = k % n; 
+        for (int i = 0; i < n; i++) {
+            cout << arr[(n - k + i) % n] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}

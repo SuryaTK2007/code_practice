c564cd2a376428c5c9c6a513183c5fe5dc748766
Surya <suryatk2007@gmail.com>
2025-09-02 23:44:21 +0530

    322. Coin Change Approach: Memo

--- 322.cpp
+++ 322.cpp
@@ -3,21 +3,27 @@ using namespace std;
 
 class Solution {
 public:
-    int helper(int i, int remaining, vector<int>& coins) {
+    int helper(int i, int remaining, vector<int>& coins, vector<vector<int>>&memo) {
         if (remaining == 0) return 0;              
         if (remaining < 0 || i >= (int)coins.size()) return -1;
-        int take = helper(i, remaining - coins[i], coins);
+        if (memo[i][remaining] != -2) return memo[i][remaining];
+        int take = helper(i, remaining - coins[i], coins, memo);
         if (take != -1) take += 1;   
-        int skip = helper(i + 1, remaining, coins);
-        if (take == -1 && skip == -1) return -1;
-        if (take == -1) return skip;
-        if (skip == -1) return take;
-        return min(take, skip);
+        int skip = helper(i + 1, remaining, coins, memo);
+        int ans;
+        if (take == -1 && skip == -1) ans=-1;
+        else if (take == -1) ans=skip;
+        else if (skip == -1) ans=take;
+        else ans=min(take, skip);
+        return memo[i][remaining]=ans;
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        return helper(0, amount, coins);
+        vector<vector<int>> memo(coins.size(), vector<int>(amount + 1, -2));
+        return helper(0, amount, coins, memo);
     }
 };
+
 int main() {
     Solution sol;
     vector<int> coins = {1, 2, 5};

94eeb47021debc8579f9036595216a9a3221be56
Surya <suryatk2007@gmail.com>
2025-09-08 14:43:38 +0530

    Approach: Two pointer

--- 1.cpp
+++ 1.cpp
@@ -3,19 +3,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> index; 
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (index.find(complement) != index.end()) {
-                return {index[complement], i};
-            }
-            index[nums[i]] = i;
+        vector<pair<int,int>> arr;
+        for (int i = 0; i < nums.size(); i++) {
+            arr.push_back({nums[i], i});
         }
-        return {}; 
+        sort(arr.begin(), arr.end());
+        int l = 0, r = arr.size() - 1;
+        while (l < r) {
+            int sum = arr[l].first + arr[r].first;
+            if (sum == target) {
+                return {arr[l].second, arr[r].second}; 
+            } else if (sum < target) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return {};
     }
 };
-
 int main() {
     vector<int> nums = {2, 7, 11, 15};
     int target = 9;

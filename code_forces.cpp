d8fefead6b9a501f3d57b4c31212248d531f5a40
Surya <suryatk2007@gmail.com>
2025-09-09 20:58:42 +0530

    Another Divisibility problem: Brute force approach

--- code_forces.cpp
+++ code_forces.cpp
@@ -1,59 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int a,b;
-    vector<int>res;
-    while(n--){
-        cin>>a>>b;
-        if(a==b) res.push_back(0);
-        else if(a%b==0 || b%a==0) res.push_back(1);
-        else res.push_back(2);
-    }
-    for(int x:res){
-        cout<<x<<" ";
+int main()
+{
+    int t;
+    cin>>t;
+    int x;
+    while(t--){
+        cin>>x;
+        string str1=to_string(x);
+        for(int i=1; i<INT_MAX; i++){
+            string str2=to_string(i);
+            string res=str1+str2;
+            int y=stoi(res);
+            int sum=x+i;
+            if(y%sum==0){
+                cout<<i<<endl;
+                break;
+            }
+        }
     }
 }
-/*
-
-Maple has two positive integers a and b
-She may perform the following operation any number of times (possibly zero) to make a equal to b
-
-Choose any positive integer x, and multiply either a or b by x
-Your task is to determine the minimum number of operations required to make a equal to b
-It can be proven that this is always possible.
-
-Input
-Each test contains multiple test cases. The first line contains the number of test cases t(1≤t≤100). 
-The description of the test cases follows.
-
-The first and only line of each test case contains two positive integers a
- and b (1≤a,b≤1000) — the numbers Maple currently has.
-
-Output
-For each test case, output a single integer representing the minimum number of operations Maple needs to make a equal to b
-
-Example
-
-Input
-3
-1 2
-10 3
-1000 1000
-
-Output
-1
-2
-0
-Note
-In the first test case, you can multiply a=1 by 2 to obtain a=b=2
-This requires one operation.
-
-In the second test case, you can multiply a=10 by 300 to get a=3000, then multiply b=3 by 1000 to get b=3000. 
-This requires two operations. Note that the numbers may exceed 1000
-after the operations.
-
-In the third test case, a and b are already equal, so no operations are required.
-
-*/

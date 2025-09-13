0b398b062ad5728fe55918567e94c7de0d6f24c0
Surya <suryatk2007@gmail.com>
2025-09-13 09:21:54 +0530

    Approach: using sorting

--- 169.cpp
+++ 169.cpp
@@ -3,22 +3,9 @@ using namespace std;
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int max_e = 0;
-        int max_count = 0;
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-            if (count > max_count) {
-                max_count = count;
-                max_e = nums[i];
-            }
-        }
-        return max_e;
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        return nums[n/2];
     }
 };
 int main() {

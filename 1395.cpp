8ce5e8dd4c15ce677b3abf082fe945f9a4071512
Surya <suryatk2007@gmail.com>
2025-09-14 11:14:27 +0530

    Approach: Divide and conquer

--- 1395.cpp
+++ 1395.cpp
@@ -3,24 +3,36 @@ using namespace std;
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        int n = rating.size();
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[j] > rating[k])) {
-                        count++;
-                    }
-                }
+        int n=rating.size();
+        //int smaller_left=0, larger_right=0;
+        int smaller_left_count=0;
+        int greater_right_count=0;
+        int greater_left_count=0;
+        int smaller_right_count=0;
+        int count=0;
+        for(int i=0; i<n; i++){
+            int mid=rating[i];
+            smaller_left_count=0;
+            greater_right_count=0;
+            greater_left_count=0;
+            smaller_right_count=0;
+            for(int j=0; j<i; j++){
+                if(rating[j]<mid) smaller_left_count++;
+                if(rating[j]>mid) greater_left_count++;
             }
+            for(int k=i+1; k<n; k++){
+                if(rating[k]>mid) greater_right_count++;
+                if(rating[k]<mid) smaller_right_count++;
+            }
+            count+=smaller_left_count*greater_right_count;
+            count+=greater_left_count*smaller_right_count;
         }
         return count;
     }
 };
 int main() {
     Solution sol;
-    vector<int> rating = {2, 1, 3};
+    vector<int> rating = {2,5,3,4,1};
     int result = sol.numTeams(rating);
     cout << "Number of teams: " << result << endl;
     return 0;

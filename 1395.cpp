ef52fb4d0b0f814d83486509d225e21fca19ffdf
Surya <suryatk2007@gmail.com>
2025-09-14 10:34:37 +0530

    Approach: Brute Force

---
+++ 1395.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        int n = rating.size();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if ((rating[i] < rating[j] && rating[j] < rating[k]) ||
+                        (rating[i] > rating[j] && rating[j] > rating[k])) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
+int main() {
+    Solution sol;
+    vector<int> rating = {2, 1, 3};
+    int result = sol.numTeams(rating);
+    cout << "Number of teams: " << result << endl;
+    return 0;
+}

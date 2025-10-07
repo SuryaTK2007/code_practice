23fe1eb292de603769843a1b5bba51e49c4f8f86
Surya <suryatk2007@gmail.com>
2025-10-07 20:05:58 +0530

    Implement string comparison ignoring case for two input strings - 112A

--- code_forces.cpp
+++ code_forces.cpp
@@ -2,12 +2,51 @@
 using namespace std;
 int main()
 {
-    int t;
-    cin>>t;
-    int x,n;
-    while(t--){
-        cin>>x>>n;
-        if(n%2==0) cout<<0<<endl;
-        else if(n%2==1) cout<<x<<endl;
-    }
+    string str1, str2;
+    cin>>str1>>str2;
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+    if(str1<str2)
+        cout<<-1;
+    else if(str1>str2)
+        cout<<1;
+    else
+        cout<<0;
 }
+
+/*
+Little Petya loves presents. His mum bought him two strings of the same size for his birthday. The strings consist of uppercase and lowercase
+Latin letters. Now Petya wants to compare those two strings lexicographically. The letters' case does not matter, that is an uppercase letter is 
+considered equivalent to the corresponding lowercase letter. Help Petya perform the comparison.
+
+Input
+Each of the first two lines contains a bought string. The strings' lengths range from 1 to 100 inclusive. It is guaranteed that the strings are 
+of the same length and also consist of uppercase and lowercase Latin letters.
+
+Output
+If the first string is less than the second one, print "-1". If the second string is less than the first one, print "1". If the strings are 
+equal, print "0". Note that the letters' case is not taken into consideration when the strings are compared.
+
+Examples
+
+Input
+aaaa
+aaaA
+
+Output
+0
+
+Input
+abs
+Abz
+
+Output
+-1
+
+Input
+abcdefg
+AbCdEfF
+
+Output
+1
+*/

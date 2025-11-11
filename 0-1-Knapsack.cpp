a7741958bb540c23ac8afa172389ab5a933bfced
Surya <suryatk2007@gmail.com>
2025-11-11 18:47:52 +0530

    Refactor knapsack helper function to use dynamic programming for improved efficiency

--- 0-1-Knapsack.cpp
+++ 0-1-Knapsack.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 class Solution {
 public:
-    int helper(int W, vector<int>& val, vector<int>& wt, int i) {
+    int helper(int W, vector<int>& val, vector<int>& wt, int i, vector<vector<int>>& dp) {
         if (i == val.size()) return 0;
+        if (dp[i][W] != -1) return dp[i][W];
         int x = val[i];
         int y = wt[i];
-        int skip = helper(W, val, wt, i + 1);
+        int skip = helper(W, val, wt, i + 1, dp);
         int take = 0;
-        if (W >= y) take = x + helper(W - y, val, wt, i + 1);
-        return max(take, skip);
+        if (W >= y) take = x + helper(W - y, val, wt, i + 1, dp);
+        return dp[i][W] = max(take, skip);
     }
     int knapsack(int W, vector<int>& val, vector<int>& wt) {
-        return helper(W, val, wt, 0);
+        int n = val.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, -1));
+        return helper(W, val, wt, 0, dp);
     }
 };
 

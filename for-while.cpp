d19ff236340265fbb127221757ae7d6bb9207378
Surya <suryatk2007@gmail.com>
2026-03-28 21:10:13 +0530

    Add for-while.cpp to demonstrate a loop with a flag

---
+++ for-while.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    bool flag=true;
+    for(int i=0;flag;i++){
+        cout<<"Hello World"<<endl;
+        if(i==5) flag=false;
+    }
+    return 0;
+}

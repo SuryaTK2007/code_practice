4f63b753753d5ec3c8e1d82a188254b1077b1c83
Surya <suryatk2007@gmail.com>
2026-07-06 10:21:34 +0530

    B - Fill the Gaps

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -4,41 +4,37 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
-    vector<vector<int>> arr(n, vector<int>(n));
-
-    for (int i = 0; i < n; i++) {
-        string s;
-        cin >> s;
-        for (int j = 0; j < n; j++) {
-            arr[i][j] = s[j] - '0';
+    vector<int> arr;
+    int x;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> x;
+        arr.push_back(x);
+    }
+    vector<int> res;
+    for (int i = 0; i < n-1; i++){
+        if(abs(arr[i+1] - arr[i])==1){
+            res.push_back(arr[i]);
+        }
+        else if(arr[i+1]>arr[i]){
+            int x = arr[i + 1];
+            int j = arr[i];
+            while(j!=x){
+                res.push_back(j);
+                j++;
+            }
+        }
+        else{
+            int x = arr[i + 1];
+            int j = arr[i];
+            while(j!=x){
+                res.push_back(j);
+                j--;
+            }
         }
     }
-    int prev = arr[0][0];
-    
-    // Left column
-    for (int i = 0; i < n - 1; i++)
-        arr[i][0] = arr[i + 1][0];
-
-    // Bottom row
-    for (int j = 0; j < n - 1; j++)
-        arr[n - 1][j] = arr[n - 1][j + 1];
-
-    // Right column
-    for (int i = n - 1; i > 0; i--)
-        arr[i][n - 1] = arr[i - 1][n - 1];
-
-    // Top row
-    for (int j = n - 1; j > 1; j--)
-        arr[0][j] = arr[0][j - 1];
-
-    arr[0][1] = prev;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << arr[i][j];
-        cout << '\n';
+    res.push_back(arr[n - 1]);
+    for (int i = 0; i < res.size(); i++){
+        cout << res[i] << " ";
     }
-
-    return 0;
 }

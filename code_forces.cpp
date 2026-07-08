5552d3e96ec94fd7c1afc622cc74d4efd139605e
Surya <suryatk2007@gmail.com>
2026-07-08 10:08:35 +0530

    B. Mark the Dust Sweeper

--- code_forces.cpp
+++ code_forces.cpp
@@ -5,21 +5,38 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, h, m;
-        cin >> n >> h >> m;
-        int current = h * 60 + m;
-        int ans = 24 * 60;
-        while (n--) {
-            int ah, am;
-            cin >> ah >> am;
-            int alarm = ah * 60 + am;
-            int diff = alarm - current;
-            if (diff < 0)
-                diff += 24 * 60;
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+            
+        long long ans = 0;
 
-            ans = min(ans, diff);
+        // Find the first non-zero element.
+        // Leading zeros don't matter because there is no dust
+        // before them to move.
+        int i = 0;
+        while (i < n && a[i] == 0)
+            i++;
+
+        // We process only till n-2.
+        // The last room can contain any amount of dust,
+        // so we never need to move dust out of it.
+        for (; i < n - 1; i++) {
+
+            // Every unit of dust must be moved once to the right.
+            // So add the amount of dust.
+            ans += a[i];
+
+            // If this room has 0 dust (after the first non-zero),
+            // we must first bring one unit of dust here from the left
+            // before continuing. This costs one extra operation.
+            if (a[i] == 0)
+                ans++;
         }
-        cout << ans / 60 << " " << ans % 60 << endl;
+
+        cout << ans << "\n";
     }
 
     return 0;

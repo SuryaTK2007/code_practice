83178dc49ddafde3aafea9bc29f613fb4e80eaad
suryatk2007 <suryatk2007@gmail.com>
2026-09-08 09:45:19 +0530

    Refactors dust movement logic to simplify input handling and scoring mechanism

--- code_forces.cpp
+++ code_forces.cpp
@@ -4,39 +4,23 @@ using namespace std;
 int main() {
     int t;
     cin >> t;
+
     while (t--) {
-        int n;
-        cin >> n;
-        vector<long long> a(n);
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-            
-        long long ans = 0;
+        int ans = 0;
 
-        // Find the first non-zero element.
-        // Leading zeros don't matter because there is no dust
-        // before them to move.
-        int i = 0;
-        while (i < n && a[i] == 0)
-            i++;
+        for (int i = 0; i < 10; i++) {
+            string s;
+            cin >> s;
 
-        // We process only till n-2.
-        // The last room can contain any amount of dust,
-        // so we never need to move dust out of it.
-        for (; i < n - 1; i++) {
-
-            // Every unit of dust must be moved once to the right.
-            // So add the amount of dust.
-            ans += a[i];
-
-            // If this room has 0 dust (after the first non-zero),
-            // we must first bring one unit of dust here from the left
-            // before continuing. This costs one extra operation.
-            if (a[i] == 0)
-                ans++;
+            for (int j = 0; j < 10; j++) {
+                if (s[j] == 'X') {
+                    int score = min({i, j, 9 - i, 9 - j}) + 1;
+                    ans += score;
+                }
+            }
         }
 
-        cout << ans << "\n";
+        cout << ans << '\n';
     }
 
     return 0;

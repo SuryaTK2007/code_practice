d0c1d995acb4c4183e62bf6b243bc51e7c68bcdb
suryatk2007 <suryatk2007@gmail.com>
2026-09-09 09:42:52 +0530

    Refactors linked list implementation in main function and removes factorial calculation

--- training.cpp
+++ training.cpp
@@ -1,14 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main()
-{
-    long long n;
-    cin>>n;
-    long long fact=1;
-    for(long long i=1;i<=n;i++)
-    {
-        fact*=i;  
+struct Node{
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=NULL;
     }
-    cout<<fact<<endl;
+};
+int main(){
+    Node* head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(3);
+    head->next->next->next=new Node(4);
+    head->next->next->next->next=new Node(5);
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    return 0;
 }
